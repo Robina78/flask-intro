88296c6f74b0dba4bdcf0f25c305406de85bb679
Ruby Hoseinzadeh <robina.sre78@gmail.com>
2021-02-16 12:35:31 -0800

    flask introduction exerceise

---
+++ calc/app.py
@@ -0,0 +1,55 @@
+# Put your app in here.
+from flask import Flask, request
+app = Flask(__name__)
+
+from operations import add, sub, mult, div
+
+@app.route("/add")
+def add_nums():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+    return str(result)
+
+
+@app.route('/sub')
+def sub_nums():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+    return str(result)   
+
+
+@app.route('/mult')
+def mult_nums():
+    a = int(request.args.get("a"))    
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+    return str(result)
+
+
+@app.route('/div') 
+def div_nums():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+    return str(result)
+
+
+
+operators = {
+        "add": add,
+        "sub": sub,
+        "mult": mult,
+        "div": div
+}
+
+@app.route('/math/<oper>')
+def math(oper):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a, b)    
+    return str(result)
+
+
+
